5d591d547467693063f00ebdca7165b75a580dba
Atomsk97 <atomsk2.0.0@gmail.com>
2021-05-26 21:37:13 -0500

    Codificación de las funciones de la ui

--- tcpserver.cpp
+++ tcpserver.cpp
@@ -6,6 +6,16 @@ TcpServer::TcpServer(QWidget *parent)
     , ui(new Ui::TcpServer)
 {
     ui->setupUi(this);
+    //Init the server
+    initServer();
+    //Random messages
+    for (int i = 0; i < 20; i++) {
+        randomMessages << "Mensaje aleatorio " + QString::number(i);
+    }
+    //connects
+    connect(ui->btn_close, &QAbstractButton::clicked, this, &QWidget::close);
+    //connect for the server socket
+    connect(qTcpServer, &QTcpServer::newConnection, this, &TcpServer::sendRandomMessage);
 }
 
 TcpServer::~TcpServer()
@@ -13,3 +23,44 @@ TcpServer::~TcpServer()
     delete ui;
 }
 
+void TcpServer::initServer()
+{
+    qInfo() << "init server\n";
+    qTcpServer = new QTcpServer(this);
+
+    if(!qTcpServer->listen()){
+        qInfo() << "Server error: " + qTcpServer->errorString();
+        return;
+    }
+
+    QString port = QString::number(qTcpServer->serverPort(), 10);
+    QString ipAddress;
+    QList<QHostAddress> ipAddressList = QNetworkInterface::allAddresses();
+    for (int i = 0; i < ipAddressList.size(); i++) {
+        if(ipAddressList.at(i) != QHostAddress::LocalHost && ipAddressList.at(i).toIPv4Address()){
+            ipAddress = ipAddressList.at(i).toString();
+            qInfo() << "ip: " << ipAddress << Qt::endl;
+            break;
+        }
+    }
+    if(ipAddressList.isEmpty()){
+        ipAddress = QHostAddress(QHostAddress::LocalHost).toString();
+    }
+
+    QString info = "ip: " + ipAddress + ", puerto: " + port;
+    ui->lbl_info->setText(info);
+}
+
+void TcpServer::sendRandomMessage()
+{
+    qInfo() << "Send random message\n";
+    QByteArray qByteArray;
+    QDataStream qDataSteam(&qByteArray, QIODevice::WriteOnly);
+    QString randomMessage = randomMessages[QRandomGenerator::global()->bounded(randomMessages.size())];
+    qDataSteam << randomMessage;
+    QTcpSocket *clientConnection = qTcpServer->nextPendingConnection();
+    clientConnection->write(qByteArray);
+    clientConnection->disconnectFromHost();
+    ui->lbl_message->setText(randomMessage);
+    connect(clientConnection, &QAbstractSocket::disconnected, clientConnection,&QObject::deleteLater);
+}
--- tcpserver.h
+++ tcpserver.h
@@ -2,6 +2,8 @@
 #define TCPSERVER_H
 
 #include <QDialog>
+#include <QTcpServer>
+#include <QtNetwork>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class TcpServer; }
@@ -17,5 +19,15 @@ public:
 
 private:
     Ui::TcpServer *ui;
+    //Socket server
+    QTcpServer *qTcpServer = nullptr;
+    //Message array
+    QVector<QString> randomMessages;
+
+    //Functions
+    void initServer();
+
+private slots:
+    void sendRandomMessage();
 };
 #endif // TCPSERVER_H
